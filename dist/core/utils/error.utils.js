d5be72d8e9628be034b1c825071cbe349ce43a5a
Igor Borisenko <ghostmania2@gmail.com>
2026-04-25 16:30:53 +0400

    Stop tracking dist build output

--- dist/core/utils/error.utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createErrorMessages = void 0;
-const createErrorMessages = (errors) => {
-    return { errorsMessages: errors };
-};
-exports.createErrorMessages = createErrorMessages;
